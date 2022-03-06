66cd992ac124e2a260df8dfed37b0c13372ae9b7
Prajwal_Adiga_15 <prajwaladiga2000@gmail.com>
2022-03-07 00:40:50 +0530

    scrapping

--- webscarpe/webscarpe/spiders/spider1.py
+++ webscarpe/webscarpe/spiders/spider1.py
@@ -1,11 +1,16 @@
 import scrapy
+#from items import GfgItem
 
 class QuotesSpider(scrapy.Spider):
     name = "webscrape"
-    start_urls = ['https://in.seamsfriendly.com/']
+    def start_requests(self):
+        urls = ['https://in.seamsfriendly.com/collections/shorts']
+        for url in urls:
+            yield scrapy.Request(url = url, callback=self.parse)
     
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'webscrape-%s.html'% page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+                title: response.css("#shopify-section-collection-template a::text").extract()
+                price: response.css("#shopify-section-collection-template .Text--subdued::text").extract()
+                img_url: response.css(".ProductItem__Image lazyautosizes Image--lazyLoaded::text").extract()
+
+
